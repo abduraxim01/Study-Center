4ef2c704792b4095cf4135c88c0675bcf3739a5e
Abduraxim <150508362+abduraxim01@users.noreply.github.com>
2024-12-25 10:03:51 +0500

    Add files via upload

---
+++ src/test/java/com/practice/StudyCenter/StudyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.StudyCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
